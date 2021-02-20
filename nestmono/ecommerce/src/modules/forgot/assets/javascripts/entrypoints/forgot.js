e20c44b5ecc646c5ae387bc841ef45390cfa140f
George Fogle <fogle.george@gmail.com>
2021-02-20 00:24:58 -0500

    :information_desk_person:

--- nestmono/ecommerce/src/modules/forgot/assets/javascripts/entrypoints/forgot.js
+++
@@ -1,1 +0,0 @@
-// This file will get bundled and included automatically in views/forgot.ejs
--- nestmono/ecommerce/src/modules/forgot/module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import ForgotController from './controllers/controller';
-import ForgotService from './services/service';
-
-@Module({
-  controllers: [ForgotController],
-  providers: [ForgotService],
-})
-export default class ForgotModule {}
